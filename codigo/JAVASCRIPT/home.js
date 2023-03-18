ffb420bb23254f3a9a69d8204c7234508c5dc035
David Sosa Santana <113687894+DavidDenoia@users.noreply.github.com>
2023-03-18 11:43:23 +0000

    Add files via upload
    Se añadió la verificación de los usuarios del JSON en el Login. Ya se puede ver el mapa en el home y se puede crear los puntos para verlos en el mapa. Cada punto redirija a lugar html con las latitudes y longitudes.

---
+++ codigo/JAVASCRIPT/home.js
@@ -0,0 +1,89 @@
+let map;
+function inicializarMapa(){
+    map = L.map('mapa').setView([28.09973, -15.41343], 10);
+
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+    maxZoom: 18
+    }).addTo(map);
+
+}
+
+
+function agregarMarcador(lat, lng, nombre, descripcion){
+    const marcador = L.marker([lat, lng]).addTo(map);
+    marcador.nombreLugar = nombre;
+    marcador.descripcionLugar=descripcion;
+
+    
+    marcador.on('mouseover', function (e) {
+        const divInfoLugar = document.getElementById('mostrarDescripcion');
+       
+        const h2Nombre = document.createElement('h2');
+        h2Nombre.innerHTML = e.target.nombreLugar;
+        divInfoLugar.appendChild(h2Nombre); 
+    
+        const pDescripcion = document.createElement('p');
+        pDescripcion.innerHTML = e.target.descripcionLugar;
+        divInfoLugar.appendChild(pDescripcion);
+    });
+
+
+    
+    marcador.on('mouseout', function () {
+        const divInfoLugar = document.getElementById('mostrarDescripcion');
+        divInfoLugar.textContent = '';
+    });
+
+    marcador.on('dblclick', function () {
+        window.location.href = `lugar.html?lat=${lat}&lng=${lng}`;
+    });
+
+    marcador.on('click', function () {
+        window.location.href = `lugar.html?lat=${lat}&lng=${lng}`;
+    });
+}
+
+function modificarHomeLogeado(){
+    const logeado=sessionStorage.getItem('login');
+    if(logeado === 'true'){
+        const usuario = JSON.parse(sessionStorage.getItem('usuario'));
+        const h2Nombre = document.createElement('h2');
+        h2Nombre.innerHTML = `Hola, ${usuario.nombre}!!!!!!!!!`;
+        mainContainer.appendChild(h2Nombre);
+
+        const enlacePerfil = document.createElement('a');
+        enlacePerfil.href = '../HTML/perfil.html';
+        enlacePerfil.textContent = 'Mi perfil';
+        mainContainer.appendChild(enlacePerfil);
+
+        const botonCerrarSesion = document.createElement('button');
+        botonCerrarSesion.textContent = 'Cerrar sesión';
+
+
+        botonCerrarSesion.onclick = function() {
+        
+            sessionStorage.removeItem('usuario');
+            sessionStorage.removeItem('login');
+
+   
+            window.location.href = 'home.html';
+        };
+
+        mainContainer.appendChild(botonCerrarSesion);
+
+    }
+
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    inicializarMapa();
+    modificarHomeLogeado();
+    agregarMarcador(28.143200398075997, -15.433784670611074, 'Las Canteras', 'Esta es una breve descripcion del sitio');
+    
+
+});
