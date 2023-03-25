296c7b2ba239663eeafaa02047572d14c6406084
Marco <marco_nehuen@hotmail.com>
2023-03-25 02:36:38 +0000

    Refactorización para merge

---
+++ codigo/JAVASCRIPT/header.js
@@ -0,0 +1,110 @@
+/*function modificarHomeLogeado(){
+    const logeado=sessionStorage.getItem('login');
+    let divInfo = document.getElementById('SignUpLogIn');
+    if(logeado === 'true'){
+        const usuario = JSON.parse(sessionStorage.getItem('usuario'));
+        const h2Nombre = document.createElement('h2');
+        h2Nombre.innerHTML = `Hola, ${usuario.nombre}!!!!!!!!!`;
+
+        const enlacePerfil = document.createElement('a');
+        enlacePerfil.href = '../HTML/perfil.html';
+        enlacePerfil.textContent = `${usuario.nombre}`;
+
+
+        divInfo.innerHTML = enlacePerfil.innerHTML;
+        divInfo.textContent = `${usuario.nombre}`;
+        divInfo.href = '../HTML/perfil.html';
+
+        console.log(divInfo);
+
+
+        const botonCerrarSesion = document.createElement('button');
+        botonCerrarSesion.textContent = 'Cerrar sesión';
+        botonCerrarSesion.id = 'SignUpLogIn2';
+
+        botonCerrarSesion.onclick = function() {
+
+            sessionStorage.removeItem('usuario');
+            sessionStorage.removeItem('login');
+
+
+            window.location.href = 'home.html';
+            console.log(window.location.href);
+        };
+        console.log(botonCerrarSesion);
+        headersection1.appendChild(botonCerrarSesion);
+
+    }
+    if(logeado === false){
+        divInfo.innerHTML = `<p>Registro | Login<p>`;
+        divInfo.href = '../HTML/registroLogin.html';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    fetch("header.html")
+        .then((response) => response.text())
+        .then((html) => {
+            const headerElement = document.createElement("header");
+            headerElement.innerHTML = html;
+
+            document.body.insertBefore(headerElement, document.body.firstChild);
+            modificarHomeLogeado();
+        })
+        .catch((error) => {
+            console.warn("Error al cargar el header:", error);
+        });
+});
+
+*/
+
+
+/*
+document.addEventListener("DOMContentLoaded", function() {
+
+    fetch("header.html")
+        .then((response) => response.text())
+        .then((html) => {
+            const headerElement = document.createElement("header");
+
+            headerElement.innerHTML = html;
+
+            document.body.insertBefore(headerElement, document.body.firstChild);
+            //modificarHomeLogeado();
+            menuhamb();
+            //modificarHomeLogeado();
+        })
+        .catch((error) => {
+            console.warn("Error al cargar el header:", error);
+        });
+});*/
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    fetch("header.html")
+        .then((response ) => response.text())
+        .then((html) => {
+            const headerElement = document.createElement("header");
+            headerElement.innerHTML = html;
+            document.body.insertBefore(headerElement, document.body.firstChild);
+            menuhamb();
+        })
+        .catch((error) => {
+            console.warn("Error al cargar el header:", error);
+        });
+
+
+});
+
+
+function menuhamb(){
+    const menu_hamb = document.getElementById("menu_hamburguesa");
+    const sidebar = document.getElementById("sidebar");
+
+    menu_hamb.addEventListener('click', function() {
+        menu_hamb.classList.toggle('is_active');
+        sidebar.classList.toggle('is_active');
+    });
+}
--- codigo/README.md
+++
@@ -1,6 +0,0 @@
-# Estructuración del códgio
-* General: 
-Nuestra web tiene 5 páginas: home (home.html), descripción del lugar (lugar.html), buscador (resultadoBusqueda.html), comentario (comentario.html) y planea tu viaje (.html).
-
-* Carga del header y footer: 
-Los cargamos de manera dinámica con lo proporcionado en clase.
