48428a288c43e9347c3486df4e02f0bbffa7c07d
David Sosa Santana <113687894+DavidDenoia@users.noreply.github.com>
2023-04-03 13:00:37 +0100

    Hecho el componente de footer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'probando'`, () => {
+  it(`should have as title 'visitGC'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('probando');
+    expect(app.title).toEqual('visitGC');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('probando app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('visitGC app is running!');
   });
 });
