51c8479db13b582df8c86cca621b7e8d5c6e93f4
Desiree05 <95355425+Desiree05@users.noreply.github.com>
2023-04-05 11:36:16 +0100

    Añadido home components (descripcion, generos)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import {DescripcionComponent} from "./descripcion/descripcion.component";
+import {GenerosComponent} from "./generos/generos.component";
+import {NgOptimizedImage} from "@angular/common";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-  
+    DescripcionComponent,
+    GenerosComponent,
     FooterComponent
-  
-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
