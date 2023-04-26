b65154efd16ffe986cc8aa4b974e60a56a12d4c2
unknown <daviddenoia72@gmail.com>
2023-04-26 10:15:37 +0100

    Click lugar en buscador --> pagina lugar

--- src/app/listado-lugares/listado-lugares.component.ts
+++ src/app/listado-lugares/listado-lugares.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Lugar } from '../lugar.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listado-lugares',
@@ -8,4 +9,9 @@ import { Lugar } from '../lugar.model';
 })
 export class ListadoLugaresComponent {
   @Input() lugar: Lugar;
+  constructor(private router:Router){}
+
+  onSubmit(){
+    this.router.navigate(['/lugar'], { queryParams: { lat:this.lugar.latitud, lng:this.lugar.longitud } });
+  }
 }
